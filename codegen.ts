34df2009090d9d1763e394bca35314a7cbebb51d
Davi Alexandre <deltreertl@gmail.com>
2023-07-23 22:02:20 -0300

    codegen for local schema

--- codegen.ts
+++ codegen.ts
@@ -1,23 +1,34 @@
 import type { CodegenConfig } from '@graphql-codegen/cli';
+import fs from 'fs';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema:
-    process.env.NODE_ENV === 'development'
-      ? 'http://localhost:3000/graphql'
-      : 'https://graph-ql-nextjs.vercel.app/graphql',
-  documents: 'app/**/*.{ts,tsx,gql,graphql}',
+  schema: 'graphql/schemas/**/*.{ts,tsx,gql,graphql}',
+  documents: [
+    'app/**/*.{ts,tsx,gql,graphql,}',
+    'src/**/*.{ts,tsx,gql,graphql,}',
+    'graphql/**/*.{ts,tsx,gql,graphql}',
+  ],
   generates: {
     'graphql/types/': {
       preset: 'client',
       plugins: [
-        // 'typescript' gives me a duplication error in /graphql/types/graphql.ts
+        // 'typescript' results in duplication error in /graphql/types/graphql.ts
         // 'typescript',
         'typescript-resolvers',
       ],
     },
   },
   watch: true,
+  hooks: {
+    afterAllFileWrite: forceReloadApolloExtension,
+  },
 };
+
+// usually there's a big delay to update the query intellisense
+function forceReloadApolloExtension() {
+  const p = './apollo.config.js';
+  fs.writeFileSync(p, fs.readFileSync(p));
+}
 
 export default config;
